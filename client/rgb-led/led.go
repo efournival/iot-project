1eabc3f49bd3e1f90e15726dbf712bcae1d69350
Edgar Fournival <contact@edgar-fournival.fr>
2018-02-22 12:36:02 +0100

    rgb led

---
+++ client/rgb-led/led.go
@@ -0,0 +1,41 @@
+package led
+
+import (
+	"fmt"
+	"os"
+
+	"periph.io/x/periph"
+	"periph.io/x/periph/conn/gpio"
+)
+
+type RGBLED struct {
+	r, g, b gpio.PinPWM
+}
+
+func init() {
+	if _, err := periph.Init(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func NewRGBLED(rpin, gpin, bpin gpio.PinIO) *RGBLED {
+	return &RGBLED{
+		rpin.(gpio.PinPWM),
+		gpin.(gpio.PinPWM),
+		bpin.(gpio.PinPWM),
+	}
+}
+
+func pwm(pin gpio.PinPWM, value uint8) {
+	if err := pin.PWM(gpio.DutyMax-gpio.Duty(float32(value/255)*float32(gpio.DutyMax)), 0); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func (led *RGBLED) SetColor(r, g, b uint8) {
+	pwm(led.r, r)
+	pwm(led.g, g)
+	pwm(led.b, b)
+}
---
+++ client/rgb-led/led_test.go
@@ -0,0 +1,46 @@
+package led
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"periph.io/x/periph"
+	"periph.io/x/periph/host/bcm283x"
+)
+
+var led *RGBLED
+
+func TestStart(t *testing.T) {
+	if _, err := periph.Init(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	led = NewRGBLED(bcm283x.GPIO17, bcm283x.GPIO18, bcm283x.GPIO27)
+}
+
+func TestRed(t *testing.T) {
+	led.SetColor(255, 0, 0)
+	time.Sleep(time.Second)
+}
+
+func TestGreen(t *testing.T) {
+	led.SetColor(0, 255, 0)
+	time.Sleep(time.Second)
+}
+
+func TestBlue(t *testing.T) {
+	led.SetColor(0, 0, 255)
+	time.Sleep(time.Second)
+}
+
+func TestWhite(t *testing.T) {
+	led.SetColor(255, 255, 255)
+	time.Sleep(time.Second)
+}
+
+func TestOff(t *testing.T) {
+	led.SetColor(0, 0, 0)
+}
